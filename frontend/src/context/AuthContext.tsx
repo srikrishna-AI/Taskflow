a54ec8dc1851c75bf2a49f10611606227726e587
Sri Krishna Garishapati <garishapatisrikrishna@gmail.com>
2026-08-04 10:18:01 +0530

    Update AuthContext.tsx

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -19,7 +19,7 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | null>(null);
 
 const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || http://13.63.52.134,
+  baseURL: http://13.63.52.134,
   headers: {
     "Content-Type": "application/json",
   },
