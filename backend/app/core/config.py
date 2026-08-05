1ba02a4c03ee20e7036b8e6933a508e114be9583
Ubuntu <ubuntu@ip-172-31-9-9.ap-south-1.compute.internal>
2026-08-05 06:38:15 +0000

    Configure EC2 deployment

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,5 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import List
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -7,17 +7,32 @@ class Settings(BaseSettings):
 
     project_name: str = "Task Management API"
     api_v1_str: str = "/api"
+
     backend_cors_origins: List[str] = [
         "https://main.d349b3a7xdbvgd.amplifyapp.com",
-        "http://localhost:3000", 
+        "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:3001", 
+        "http://localhost:3001",
         "http://127.0.0.1:3001",
-        "http://localhost:3002", 
-        "http://127.0.0.1:3002"
+        "http://localhost:3002",
+        "http://127.0.0.1:3002",
     ]
 
-   
+    # Database
+    database_url: str
+    db_host: str
+    db_port: int
+    db_name: str
+    db_user: str
+    db_password: str
+
+    # JWT
+    jwt_secret_key: str
+    jwt_refresh_secret_key: str
+    algorithm: str = "HS256"
+
+    access_token_expire_minutes: int = 30
+    refresh_token_expire_days: int = 7
 
     model_config = SettingsConfigDict(
         env_file=".env",
